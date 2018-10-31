c9b82e0bdc0b3380605b79c6774b223e077cff2b
SengMing Tan <tansengming@gmail.com>
2018-10-31 17:02:42 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,11 @@
+# TODO
+# Rails.application.routes.draw do
+#   if Rails.application.config.stripe.auto_mount
+#     mount Stripe::Engine => Rails.application.config.stripe.endpoint
+#   end
+# end
+
 Stripe::Subscribe::Engine.routes.draw do
+  resources :plans,   only: :index
+  resource  :payment, only: [:new, :create]
 end
